8ebd4c68bda0d1a6f1eef161b3ef257553ac9b6b
ADMIN <ADMIN@DESKTOP-CI4SVFI>
2025-02-21 16:18:53 +0700

    Add project files.

---
+++ Data/StudentContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+//using System.Data.Entity;//winform
+using Microsoft.EntityFrameworkCore;//webapi
+using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using ThuchanhAPIvoinetcore.Model;
+
+
+namespace ett1_web_api.Data
+{
+    public class StudentContext : DbContext
+    {
+        // public StudentContext() : base("name=StudentDBConnectionString") //winform
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) //webapi
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
